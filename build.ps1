6ce4bbd920c48dd40cd5930261c67b24dc752c10
AuroraZiling <auroraziling@gmail.com>
2024-10-30 19:44:33 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,11 +1,13 @@
 param(
     [string] $Architecture = "x64",
-    [string] $Version = "1.0.0.0"
+    [string] $Version = "1.1.0.0"
 )
 
 $ErrorActionPreference = "Stop";
 
-dotnet publish ./PipManager.Cli.csproj -c Release -r "win-$Architecture" -o "build/$Version/singleWithRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=true -p:EnableCompressionInSingleFile=true -p:PublishSingleFile=true -p:SelfContained=true -p:AssemblyVersion=$Version -p:Configuration=Release;
+Write-Output "Start building singleWithRuntime...";
+
+dotnet publish src/PipManager.Cli.csproj -c Release -r "win-$Architecture" -o "build/$Version/singleWithRuntime" -p:Platform=$Architecture -p:PublishReadyToRun=true -p:EnableCompressionInSingleFile=true -p:PublishSingleFile=true -p:SelfContained=true -p:AssemblyVersion=$Version -p:Configuration=Release;
 
 Write-Output "Build Finished";
 
